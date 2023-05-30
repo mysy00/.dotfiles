964c1ff454017b202a217b8c2716dbbd404dc135
Mikołaj Szczepański <pm@mysy.pl>
2023-05-30 17:08:39 +0200

    Migrate to chezmoi

---
+++ dot_config/fish/config.fish
@@ -0,0 +1,35 @@
+fish_add_path $XDG_DATA_HOME/cargo/bin
+fish_add_path $HOME/.local/bin
+
+# Import additional files
+source $XDG_CONFIG_HOME/fish/aliases.fish
+
+# Volta
+set -gx VOLTA_HOME "$XDG_CACHE_HOME/.volta"
+set -gx PATH "$VOLTA_HOME/bin" $PATH
+
+# Fix flatpak apps not being recognized
+if command -vq flatpak
+    set -ga fish_user_paths ~/.local/share/flatpak/exports/bin /var/lib/flatpak/exports/bin
+    set -gx --path XDG_DATA_DIRS /usr/local/share/ /usr/share/ ~/.local/share/flatpak/exports/share
+    for install_dir in (flatpak --installations)
+        set -gxa XDG_DATA_DIRS $install_dir/exports/share
+    end
+end
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+
+    # Custom Prompt
+    set fish_greeting
+    starship init fish | source
+
+    if test -e ~/.cache/wal/colors.fish
+        source ~/.cache/wal/colors.fish
+    end
+
+    if test -e ~/.cache/wal/sequences
+        cat ~/.cache/wal/sequences
+    end
+
+end
---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,28 @@
+local wezterm = require 'wezterm'
+local act = wezterm.action
+
+return {
+  font = wezterm.font 'Hack Nerd Font',
+  --font = wezterm.font 'SauceCodePro Nerd Font',
+  default_prog = { '/usr/bin/fish' },
+  color_scheme = "IR_Black",
+  hide_tab_bar_if_only_one_tab = true,
+  keys = {
+    -- Tabs
+    { key = '{', mods = 'CTRL|SHIFT', action=wezterm.action{ActivateTabRelative=-1} },
+    { key = '}', mods = 'CTRL|SHIFT', action=wezterm.action{ActivateTabRelative=1} },
+
+    -- Panes
+    { key = 'b', mods = 'CTRL', action = act.RotatePanes 'CounterClockwise' },
+    { key = 'n', mods = 'CTRL', action = act.RotatePanes 'Clockwise' },
+    { key = '0', mods = 'CTRL', action = act.PaneSelect { mode = 'SwapWithActive' } },
+    { key = '"', mods = 'CTRL|SHIFT', action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}} },
+    { key = '%', mods = 'CTRL|SHIFT', action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}} },
+    { key = 'w', mods = 'CTRL', action=wezterm.action{CloseCurrentPane={confirm=true}} },
+
+    -- Copy/Paste
+    { key="v",   mods="SHIFT|CTRL",     action = act.PasteFrom 'Clipboard'},
+    { key="c",   mods="SHIFT|CTRL",     action = act.CopyTo 'Clipboard'},
+
+  },
+}
