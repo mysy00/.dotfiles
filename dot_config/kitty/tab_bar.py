89435ffcc413f101fabcecd287661f305a5d4807
Mysy00 <pm@mysy.pl>
2024-08-05 07:37:09 +0200

    New look for kitty

--- dot_config/kitty/tab_bar.py
+++ dot_config/kitty/tab_bar.py
@@ -1,55 +1,22 @@
-import json
-import subprocess
-from collections import defaultdict
-from datetime import datetime, timezone
+import datetime
 
-from kitty.boss import get_boss
-from kitty.fast_data_types import Screen, add_timer
-from kitty.rgb import Color
-from kitty.tab_bar import (
-    DrawData,
-    ExtraData,
-    Formatter,
-    TabBarData,
-    as_rgb,
-    draw_attributed_string,
-    draw_title,
-)
+from kitty.fast_data_types import Screen, get_options
+from kitty.tab_bar import (DrawData, ExtraData, TabBarData, as_rgb, draw_title)
 from kitty.utils import color_as_int
 
-timer_id = None
+opts = get_options()
 
-ICON = "  "
-RIGHT_MARGIN = 1
-REFRESH_TIME = 15
+DEFAULT_BG = as_rgb(color_as_int(opts.background))
 
-icon_fg = as_rgb(color_as_int(Color(255, 250, 205)))
-icon_bg = as_rgb(color_as_int(Color(47, 61, 68)))
-bat_text_color = as_rgb(0x999F93)
-clock_color = as_rgb(0x7FBBB3)
-dnd_color = as_rgb(0x465258)
-sep_color = as_rgb(0x999F93)
-utc_color = as_rgb(color_as_int(Color(113, 115, 116)))
+ICON: str = ""
+ICON_LENGTH: int = len(ICON)
+ICON_FG = as_rgb(color_as_int(opts.color3))
+ICON_BG = DEFAULT_BG
 
-
-def calc_draw_spaces(*args) -> int:
-    length = 0
-    for i in args:
-        if not isinstance(i, str):
-            i = str(i)
-        length += len(i)
-    return length
-
-
-def _draw_icon(screen: Screen, index: int) -> int:
-    fg, bg = screen.cursor.fg, screen.cursor.bg
-    screen.cursor.fg = icon_fg
-    screen.cursor.bg = icon_bg
-    screen.draw(ICON)
-    screen.cursor.fg, screen.cursor.bg = fg, bg
-    screen.cursor.x += len(ICON)
-    return screen.cursor.x
-
+CLOCK_FG = as_rgb(color_as_int(opts.color15))
+CLOCK_BG = DEFAULT_BG
+DATE_FG = as_rgb(color_as_int(opts.color15))
+DATE_BG = DEFAULT_BG
 
 def _draw_left_status(
     draw_data: DrawData,
@@ -59,57 +26,65 @@ def _draw_left_status(
     max_title_length: int,
     index: int,
     is_last: bool,
-    extra_data: ExtraData,
+    extra_data: ExtraData
 ) -> int:
     if draw_data.leading_spaces:
         screen.draw(" " * draw_data.leading_spaces)
 
-    draw_title(draw_data, screen, tab, index)
+    if tab.is_active:
+        draw_title(draw_data, screen, tab, index)
+    else:
+        screen.cursor.bg = DEFAULT_BG
+        draw_title(draw_data, screen, tab, index)
+
     trailing_spaces = min(max_title_length - 1, draw_data.trailing_spaces)
     max_title_length -= trailing_spaces
     extra = screen.cursor.x - before - max_title_length
     if extra > 0:
         screen.cursor.x -= extra + 1
+        # Don't change `ICON`
+        screen.cursor.x = max(screen.cursor.x, ICON_LENGTH)
         screen.draw("…")
+
     if trailing_spaces:
         screen.draw(" " * trailing_spaces)
-    end = screen.cursor.x
+
     screen.cursor.bold = screen.cursor.italic = False
     screen.cursor.fg = 0
     if not is_last:
         screen.cursor.bg = as_rgb(color_as_int(draw_data.inactive_bg))
         screen.draw(draw_data.sep)
+
     screen.cursor.bg = 0
-    return end
+    return screen.cursor.x
 
 
+def _draw_right_status(screen: Screen, is_last: bool) -> int:
+    if not is_last:
+        return screen.cursor.x
 
-def _draw_right_status(screen: Screen) -> int:
-    draw_attributed_string(Formatter.reset, screen)
+    cells = [
+        (CLOCK_FG, CLOCK_BG, datetime.datetime.now().strftime(" %H:%M ")),
+        (DATE_FG, DATE_BG, datetime.datetime.now().strftime(" %Y/%m/%d ")),
+    ]
 
-    clock = datetime.now().strftime("%H:%M")
-    utc = datetime.now(timezone.utc).strftime(" (UTC %H:%M)")
-
-    cells = []
-
-    cells.append((clock_color, clock))
-    cells.append((utc_color, utc))
-
-    right_status_length = RIGHT_MARGIN
-    for cell in cells:
-        right_status_length += len(str(cell[1]))
+    right_status_length = 0
+    for _, _, cell in cells:
+        right_status_length += len(cell)
 
     draw_spaces = screen.columns - screen.cursor.x - right_status_length
-
     if draw_spaces > 0:
+        screen.cursor.bg = DEFAULT_BG
         screen.draw(" " * draw_spaces)
 
+    for fg, bg, cell in cells:
+        screen.cursor.fg = fg
+        screen.cursor.bg = bg
+        screen.draw(cell)
     screen.cursor.fg = 0
-    for color, status in cells:
-        screen.cursor.fg = color
-        screen.draw(status)
     screen.cursor.bg = 0
 
+    screen.cursor.x = max(screen.cursor.x, screen.columns - right_status_length)
     return screen.cursor.x
 
 
@@ -123,12 +98,6 @@ def draw_tab(
     is_last: bool,
     extra_data: ExtraData,
 ) -> int:
-
-    # Only draw the icon for the first tab
-    if index == 0:
-        _draw_icon(screen, index)
-
-    # Draw the left status for each tab
     end = _draw_left_status(
         draw_data,
         screen,
@@ -137,12 +106,10 @@ def draw_tab(
         max_title_length,
         index,
         is_last,
-        extra_data,
+        extra_data
     )
-
-    # Draw the right status only for the last tab, without creating a new tab
-    if is_last:
-        screen.cursor.x = screen.columns - RIGHT_MARGIN
-        _draw_right_status(screen)
-
+    _draw_right_status(
+        screen,
+        is_last,
+    )
     return end
